e2412a21e869d48db13732b309f367b5487c2685
vadim.ch <ivadik05@gmail.com>
2017-09-09 15:09:47 +0300

    fix readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # webworker-promisify
+
+webworker-promisify is wrapper over a Webworker that allows to execute a function in a separate thread and returned Promise.
+
+## How to use
+
+`npm install webworker-promisify`
+
+## Examples
+
+
+### Promise
+
+```js
+
+```
+
+### Async await
+
+```js
+
+```
+
+### @decorators
+
+```js
+
+```
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
-export function webworkerPromisify<T>(fn: Function): (data: any) => Promise<T> {
-  const workerHandler = (fn) => {
-    onmessage = (ev) => {
-      postMessage(fn(...ev.data), null);
-    };
-  };
-
-  const worker = new Worker(
-      URL.createObjectURL(
-          new Blob(['('+workerHandler+')('+fn+')'])
-      )
-  );
-
+export function webworkerPromisify<T>(fn: Function): (data?: any) => Promise<T> {
   return (...data) => {
+    const workerHandler = (fn) => {
+      onmessage = (ev) => {
+        postMessage(fn(...ev.data), null);
+      };
+    };
+
+    const worker = new Worker(
+        URL.createObjectURL(
+            new Blob(['('+workerHandler+')('+fn+')'])
+        )
+    );
+
     return new Promise((resolve, reject) => {
       worker.postMessage(data);
-      worker.onmessage = (ev) => {
-        resolve(ev.data);
+      worker.onmessage = (ev: MessageEvent) => {
         worker.terminate();
+        resolve(ev.data);
+      };
+      worker.onerror = (err: ErrorEvent) => {
+        worker.terminate();
+        reject(err.message);
       }
     });
   }
